1265f541f96b492ea17ef1108a30ff394e05905d
Anton Zotov <azpk360@yandex.ru>
2017-02-28 14:30:29 +0500

    Update quicksort.cpp

--- Quicksort/quicksort.cpp
+++ Quicksort/quicksort.cpp
@@ -1,53 +1,36 @@
 #include <iostream>
-#include <clocale>
 #include <ctime>
+#include <clocale>
+#include "sorting_func.h"
 
 using namespace std;
 
-void quicksort(int *mass, int size, int left, int right)
+/* Прогргамма быстрой сортировки массива.
+Выбирается опорный элемент массива, расположенный по центру подмассива.
+Перемещение всех элементов массива >= опорного - вправо, а все элементы < опорного - влево.
+Левый подмассив больше правого подмассива.
+Пока в подмассиве больше 2 элементов, рекурсивно повторяем послендовательность действий для подмассива. */
+int main()
 {
-	int middle = size >> 1;
-		int l = left;
-		int r = right;
-		do
-		{
-			while (mass[l] < mass[middle])
-				l++;
-			while (mass[r] > mass[middle]) 
-				r--;
-			if (l <= r)
-			{
-				mass[l] = mass[l] ^ mass[r];
-				mass[r] = mass[r] ^ mass[l];
-				mass[l] = mass[l] ^ mass[r];
-				l++;
-				r--;
-			}
-		} while (l <= r);
-		if (l < r)
-			quicksort(mass, size, l, right);
+	setlocale(LC_ALL, "Russian");								// Консоль на русском языке.
+	srand(time(0));												// Обновление генератора случайных чисел.
+	int num;													// Кол-во элементов массива.
 
-		if (l < r)
-			quicksort(mass, size, left, r);
-}
+	cout << "Введите кол-во элементов массива." << endl;
+	cin >> num;
 
-int main() 
-{
-	srand(time(NULL));
-	setlocale(LC_ALL, "Russian");							 // Русский язык в консоли
-	int *mass = new int[10];
-	for (int i = 0; i < 10; i++)
-	{
-		mass[i] = rand() % 10;
-		cout << mass[i] << endl;
-	}
-	quicksort(mass, 9, 0, 9);
-	cout << "--------------------" << endl;
-	for (int i = 0; i < 10; i++)
-	{
-		mass[i] = rand() % 10;
-		cout << mass[i] << endl;
-	}
-	system("pause");
+	int *mass = new int[num];									// Создание динамического массива
+
+	rand_mass(mass, num);										// Заполнение массива случайными числами.
+	print_mass(mass, num);
+
+	quicksort(mass, num-1, 0, num-1);						    // Быстрая сортировка массива
+
+	cout << endl << "Сортированный массив." << endl;
+
+	print_mass(mass, num);
+
+	delete [] mass;												// Удаление динамического массива.
+	system("pause");											// Задержка консоли.
 	return 0;
 }
