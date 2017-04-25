51a8d73d90a59ca21b028c69054711afc2ecdb2e
Anton Zotov <azpk360@yandex.ru>
2017-04-25 11:35:12 +0500

    Update sorting_func.h

--- HeapSort/sorting_func.h
+++ HeapSort/sorting_func.h
@@ -4,7 +4,8 @@ void rand_mass(int*, int);
 /* Вывод массива в консоль. */
 void print_mass(int*, int);
 
-/* Ïîìåíÿòü ìåñòàìè ýëåìåíòû ìàññèâà, ñòîÿùèå ðÿäîì. */
+/* Поменять местами i-ый элемент массива, и следующий за ним. */
 void change(int*, int, int);
+
 
 void heapsort(int*, int);
